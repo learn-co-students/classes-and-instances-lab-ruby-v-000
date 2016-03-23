d5597ea9e1fb603b2827b82515d44fdb848e432d
hilaryml <hilyttle@gmail.com>
2016-03-23 18:08:22 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog  #adds a class definition for Dog
+end
+
+fido = Dog.new  #instantiates ('brings to life') a new instance (individual object produced by Dog class)
+snoopy = Dog.new
+lassie = Dog.new
