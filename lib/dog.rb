dc0c702b7dec6f5c7ab36190f8d55e9e3d325407
machen2 <victoriamachen@gmail.com>
2016-11-15 02:00:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+#class definition for Dog class
+class Dog
+end
+
+#create three dogs in local variables
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+#class definition
+class Person
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
