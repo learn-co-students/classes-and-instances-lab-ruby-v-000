b69f057fafa773ad22ac99beba093bed1d433d46
Nicholas D'Amico <nickalan82@icloud.com>
2017-06-03 14:41:30 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+  # Our Dog Object Data and Methods go here
+end
+
+  fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  # Data and Methods go here
+end
+
+adele_goldberg = Person.new
+      alan_kay = Person.new
