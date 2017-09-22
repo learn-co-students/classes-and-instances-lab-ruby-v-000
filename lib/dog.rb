c8ad41c761249f5c31c93a16a76b1d76ad1b8224
YanChan (Yona) Lin <yonalin1@gmail.com>
2017-09-22 17:03:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  # snoppy = Dog.new
+  # fido = Dog.new
+  # lassie = Dog.new
+end
+fido = Dog.new
+fido
+
+snoopy = Dog.new
+snoopy
+
+lassie = Dog.new
+lassie
+ # Dog = ["snoppy","fido","lassie"]
