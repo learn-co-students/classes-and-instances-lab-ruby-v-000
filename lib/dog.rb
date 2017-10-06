f3d0a94231f21112c6b32dfd8b423b517bcffcbe
Blakeley Bond <burrick2003@yahoo.com>
2017-10-06 02:18:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog
+tail = "fat"
+end
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,5 @@
+class Person
+  favorite_food = "Pizza"
+end
+adele_goldberg = Person.new
+alan_kay = Person.new
