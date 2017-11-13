d413db05d7776466eea67a960fd7ff15feaedf22
James Dolan <jwdolan820@gmail.com>
2017-11-13 18:34:52 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,6 @@
-class Dog
+class Dog #Dog class definition
 end
 
-fido = Dog.new
+fido = Dog.new   #creates three dogs in local variables
 snoopy = Dog.new
 lassie = Dog.new
-
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,7 @@
-class Person
+class Person #Person class definition
 end
+
 
 adele_goldberg = Person.new
 alan_kay = Person.new
+#creates two person objects as local variables
