5db7e092e7a98fa67c2b93d273e6ba63c2eeb6db
Carolina Gomez-Montoya <gomezmontoya77@gmail.com>
2018-01-13 17:51:22 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+# add a class definition for a `Person` class
+class Person
+end
+# create two people in local variables, `adele_goldberg` and `alan_kay`
+adele_goldberg = Person.new
+alan_kay = Person.new
